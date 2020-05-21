d0367a4d2930abf726a1bb182890a686f33437cf
DevRed91 <51876334+DevRed91@users.noreply.github.com>
2020-05-21 13:09:38 +0530

    V1

---
+++ extensions/dockingPanelQuantity.js
@@ -0,0 +1,164 @@
+
+class DockingQuantityButton extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+        this._group = null;
+        this._button = null;
+        this._modelData = {};
+    }
+
+    load() {
+        console.log('DockingQuantityButton has been loaded');
+        return true;
+    }
+
+    unload() {
+        // Clean our UI elements if we added any
+        if (this._group) {
+            this._group.removeControl(this._button);
+            if (this._group.getNumberOfControls() === 0) {
+                this.viewer.toolbar.removeControl(this._group);
+            }
+        }
+        console.log('DockingQuantityButton has been unloaded');
+        return true;
+    }
+
+    getAllLeafComponents(callback) {
+        // from https://learnforge.autodesk.io/#/viewer/extensions/panel?id=enumerate-leaf-nodes
+        viewer.getObjectTree(function (tree) {
+            var leaves = [];
+            tree.enumNodeChildren(tree.getRootId(), function (dbId) {
+                if (tree.getChildCount(dbId) === 0) {
+                    leaves.push(dbId);
+                }
+            }, true);
+            callback(leaves);
+        });
+    }
+    
+    dynamicColors(count){
+        let background = [];
+        let borders = [];
+        for (let i = 0; i < count; i++){
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+        
+            background.push('rgba(' + r + ', ' + g + ', '+ b + ', 0.7)');
+            borders.push('rgba(' + r + ', ' + g + ', '+ b + ', 0.7)');
+        } 
+        return {background:background, borders:borders};
+    }
+
+    onToolbarCreated() {
+        // Create a new toolbar group if it doesn't exist
+        this._group = this.viewer.toolbar.getControl('AwesomeExtensionsToolbar');
+        if (!this._group) {
+            this._group = new Autodesk.Viewing.UI.ControlGroup('AwesomeExtensionsToolbar');
+            this.viewer.toolbar.addControl(this._group);
+        }
+
+        // Add a new button to the toolbar group
+        this._button = new Autodesk.Viewing.UI.Button('DockingQuantityButton');
+        this._button.onClick = (ev) => {
+            // // Check if the panel is created or not
+            if (this._panel == null) {
+                this._panel = new MyQuantityPanel(this.viewer, this.viewer.container, 'quantityChartPanel', 'Quantity Chart');
+            }
+            // Show/hide docking panel
+            this._panel.setVisible(!this._panel.isVisible());
+
+            // If panel is NOT visible, exit the function
+            if (!this._panel.isVisible())
+                return;
+
+            let ctx = document.getElementById('barChart');
+                
+                this.getAllLeafComponents((dbIds) => {
+                    let count = dbIds.length;
+                    dbIds.forEach((dbId) => {
+                        this.viewer.getProperties(dbId, (props) => {
+                            props.properties.forEach((prop) => {
+
+                                if (!isNaN(prop.displayValue)) return; // let's not categorize properties that store numbers
+
+                                // // some adjustments for revit:
+                                prop.displayValue = prop.displayValue.replace('Revit ', ''); // remove this Revit prefix
+                                
+                                // //console.log(prop.displayValue);
+                                if (prop.displayValue.indexOf('<') == 0) return; // skip categories that start with <
+
+                                // //console.log(prop);
+                                // // ok, now let's organize the data into this hash table
+                                if (this._modelData[prop.displayName] == null) this._modelData[prop.displayName] = {};
+                                if (this._modelData[prop.displayName][prop.displayValue] == null) this._modelData[prop.displayName][prop.displayValue] = [];
+                                this._modelData[prop.displayName][prop.displayValue].push(dbId);
+
+                                let getLabels = (propertyName) => {
+                                    return Object.keys(this._modelData[propertyName]);
+                                }
+                                let getCountInstances = (propertyName) => {
+                                        return Object.keys(this._modelData[propertyName]).map((key) => this._modelData[propertyName][key].length);
+                                }
+
+                                if (prop.displayName === 'Category'){
+                                   if(prop.displayValue !== 'Topography'){
+                                        // console.log(prop);
+                                        // console.log(prop.displayName, prop.displayValue.replace('Revit', ''));
+                                        // console.log(Object.keys(this._modelData[prop.displayName]));
+                                        // console.log(Object.keys(this._modelData[prop.displayName][prop.displayValue]));
+                                        // console.log(Object.keys(this._modelData[prop.displayName]).map((key) => this._modelData[prop.displayName][key].length));
+                                        let dataSet = getCountInstances(prop.displayName);
+                                        console.log(dataSet);
+                                        let colors = this.dynamicColors(getLabels(prop.displayName).length);
+                                        new Chart(ctx, {
+                                                // The type of chart we want to create
+                                                type: 'bar',
+                                    
+                                                // The data for our dataset
+                                                data: {
+                                                    labels: getLabels(prop.displayName),
+                                                    datasets: [{
+                                                        label: 'Space Demarcation',
+                                                        backgroundColor: colors.background,
+                                                        borderColor: colors.borders,
+                                                        borderWidth: 1,
+                                                        data: dataSet
+                                                    }]
+                                                },
+                                    
+                                                // Configuration options go here
+                                                options: {
+                                                    scales: {
+                                                        yAxes: [{
+                                                            ticks: {
+                                                                beginAtZero: true
+                                                            }
+                                                        }]
+                                                    }
+                                                },
+                                                legend: {
+                                                    display: false
+                                                },
+                                            });
+                                   }
+
+                                }
+                                
+
+                            })
+
+                                // if ((--count) == 0) callback();
+                        })
+                    })
+                })
+
+        };
+        this._button.setToolTip('Quantity Extension');
+        this._button.addClass('dockingQuantityIcon');
+        this._group.addControl(this._button);
+    }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('DockingQuantityButton', DockingQuantityButton);
